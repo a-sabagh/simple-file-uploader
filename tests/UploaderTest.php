4eae0c05d4a6828db1ec35f551e61475389df9a5
asabagh <a.sabagh72@gmail.com>
2019-02-19 16:48:13 +0330

    using data provider

--- tests/UploaderTest.php
+++ tests/UploaderTest.php
@@ -5,28 +5,21 @@ use SimpleUploader\Uploader;
 
 class Example extends TestCase
 {
-	private $file;
 	private $destination = __DIR__ . "/uploads";
 	protected $handle;
 	
 	public function setUp()
 	{
-		$this->file = [
-		    'name' => 'image.png',
-            'type' => 'image/png',
-            'tmp_name' => __DIR__ . "/tmp/image.png",
-            'error' => 0,
-            'size' => 98174
-		];
 		$this->handle = new Uploader($this->destination);
 	}
     /**
-	 * test single upload file
+	 * upload method test
+	 * @dataProvider UploadProvider
 	 * @test
 	 */
-    public function singleUpload()
+    public function upload($file)
 	{
-		$result = $this->handle->upload($this->file);
+		$result = $this->handle->upload($file);
 		$messages = $this->handle->getMessages();
 		for($i=0;$i<count($result);$i++):
 			$this->assertArrayHasKey('success',$messages[$i]);
@@ -34,38 +27,43 @@ class Example extends TestCase
 		endfor;
     }
     /**
-	 * test multiple upload file
-	 * @test
+	 * provider for upload test
 	 */
-	public function multipleUpload()
+	public function UploadProvider()
 	{
+		$file = [
+		    'name' => 'image.png',
+            'type' => 'image/png',
+            'tmp_name' => __DIR__ . "/tmp/image.png",
+            'error' => 0,
+            'size' => 98174
+		];
+		$singleFile = $file;
 		$multiFile = [
 			'name' => [
-				$this->file['name'],
-				$this->file['name']
+				$file['name'],
+				$file['name']
 			],
 			'type' => [
-				$this->file['type'],
-				$this->file['type']
+				$file['type'],
+				$file['type']
 			],
 			'tmp_name' => [
-				$this->file['tmp_name'],
-				$this->file['tmp_name']
+				$file['tmp_name'],
+				$file['tmp_name']
 			],
 			'error' => [
-				$this->file['error'],
-				$this->file['error']
+				$file['error'],
+				$file['error']
 			],
 			'size' => [
-				$this->file['size'],
-				$this->file['size']
+				$file['size'],
+				$file['size']
 			]
 		];
-		$result = $this->handle->upload($multiFile);
-		$messages = $this->handle->getMessages();
-		for($i=0;$i<count($result);$i++):
-			$this->assertArrayHasKey('success',$messages[$i]);
-			unlink($this->destination . $result[$i]['path']);
-		endfor;
+		return [
+			[$singleFile],
+			[$multiFile]
+		];
 	}
 }
